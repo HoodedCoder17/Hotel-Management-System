ddb727092db3a33d542de4a25f386de28f9ad5c4
HoodedCoder17 <112750436+HoodedCoder17@users.noreply.github.com>
2023-05-07 17:57:59 +0530

    UI fix for Mobile devices

--- src/main/java/com/hms/controllers/HomeController.java
+++ src/main/java/com/hms/controllers/HomeController.java
@@ -3,8 +3,6 @@ package com.hms.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.annotation.PostConstruct;
-
 @Controller
 public class HomeController {
 
