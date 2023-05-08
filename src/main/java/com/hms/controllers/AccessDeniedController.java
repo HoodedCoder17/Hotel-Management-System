e4d616f70502e6ce25168773edfdc01aefe4a1fe
HoodedCoder17 <112750436+HoodedCoder17@users.noreply.github.com>
2023-05-08 13:59:26 +0530

    Added 403 Handler

--- src/main/java/com/hms/controllers/AccessDeniedController.java
+++ src/main/java/com/hms/controllers/AccessDeniedController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccessDeniedController {
 
-	@GetMapping("/error/userRestrictedPage")
+	@GetMapping("/userRestrictedPage")
 	public String getAccessDenied() {
 		return "userRestrictedPage";
 	}
